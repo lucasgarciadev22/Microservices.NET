d2151894bbd89f821ac2daaf98c12be4d170371b
Lucas Garcia <99772659+lucasgarciadev22@users.noreply.github.com>
2023-05-23 22:11:50 -0300

    I/O commands for Catalog
    Implemented Create,Update,Delete commands;
    Implemented Specific Mapping Profiles;

--- Services/Catalog/Catalog.Application/Commands/UpdateProductCommand.cs
+++ Services/Catalog/Catalog.Application/Commands/UpdateProductCommand.cs
@@ -1,19 +1,22 @@
+using Catalog.Core.Entities;
+using MediatR;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
-namespace Catalog.Core.Entities
+namespace Catalog.Application.Commands
 {
-  public class Product : BaseEntity
+  public class UpdateProductCommand : IRequest<bool>
   {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
     [BsonElement("Name")]
     public string Name { get; set; }
     public string Summary { get; set; }
     public string Description { get; set; }
     public string ImageFile { get; set; }
+    public decimal Price { get; set; }
     public ProductBrand Brand { get; set; }
     public ProductType Type { get; set; }
-
-    [BsonRepresentation(BsonType.Decimal128)]
-    public decimal Price { get; set; }
   }
 }
---
+++ Services/Catalog/Catalog.Application/Handlers/DeleteProductByIdHandler.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.Application.Queries;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Handlers
+{
+    internal class DeleteProductByIdHandler : IRequestHandler<DeleteProductByIdQuery, bool>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public DeleteProductByIdHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<bool> Handle(DeleteProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _productRepository.DeleteProduct(request.Id);
+        }
+    }
+}
