56f9b25a17a42e1d34f4f489ef3482087cca0ed9
Lucas Garcia <99772659+lucasgarciadev22@users.noreply.github.com>
2023-07-18 20:58:59 -0300

    core: implemented Discount.Core base

---
+++ Services/Discount/Discount.Core/Repositories/IDiscountRepository.cs
@@ -0,0 +1,12 @@
+﻿using Discount.Core.Entities;
+
+namespace Discount.Core.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string poductName);
+        Task<Coupon> CreateDiscount(Coupon coupon);
+        Task<Coupon> GetDiscount(Coupon coupon);
+        Task<Coupon> DeleteDiscount(string poductName);
+    }
+}
