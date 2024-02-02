ea7e4c681fcb813da12238580fa3360071ed6591
Lucas Garcia <lucasgarciaz2018@gmail.com>
2024-02-02 19:51:36 -0300

    feat: implemented repositories

---
+++ CleanArchitecture/Movies/Movies.Core/Repositories/Base/IMovieRepository.cs
@@ -0,0 +1,8 @@
+﻿using Movies.Core.Entities;
+
+namespace Movies.Core.Repositories.Base;
+
+public interface IMovieRepository : IRepository<Movie>
+{
+    Task<IEnumerable<Movie>> GetMoviesByDirectorName(string directorName);
+}
