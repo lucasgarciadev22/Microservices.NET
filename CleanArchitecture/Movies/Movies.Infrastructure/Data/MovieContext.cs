73d5a852f85230f45eceafcae9e9f414501b2cb8
Lucas Garcia <lucasgarciaz2018@gmail.com>
2024-02-03 11:35:47 -0300

    feat: added context and seed

---
+++ CleanArchitecture/Movies/Movies.Infrastructure/Data/MovieContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.Core.Entities;
+
+namespace Movies.Infrastructure.Data;
+
+public class MovieContext(DbContextOptions<MovieContext> options) : DbContext(options)
+{
+    public DbSet<Movie> Movies { get; set; }
+}
---
+++ CleanArchitecture/Movies/Movies.Infrastructure/Data/MovieContextSeed.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.Logging;
+using Movies.Core.Entities;
+
+namespace Movies.Infrastructure.Data;
+
+public class MovieContextSeed
+{
+    public static IEnumerable<Movie> Movies => GetMovies();
+
+    public static async Task SeedAsync(
+        MovieContext movieContext,
+        ILoggerFactory loggerFactory,
+        int retry = 0
+    )
+    {
+        int retryForAvailability = retry;
+        try
+        {
+            await movieContext.Database.EnsureCreatedAsync();
+
+            if (!movieContext.Movies.Any())
+            {
+                movieContext.Movies.AddRange(GetMovies());
+                await movieContext.SaveChangesAsync();
+            }
+        }
+        catch (Exception ex)
+        {
+            if (retryForAvailability < 3)
+            {
+                retryForAvailability++;
+                var log = loggerFactory.CreateLogger<MovieContextSeed>();
+
+                log.LogError("Exception occured while connecting:{Message}", ex.Message);
+
+                await SeedAsync(movieContext, loggerFactory, retryForAvailability);
+            }
+        }
+    }
+
+    private static IEnumerable<Movie> GetMovies()
+    {
+        return [
+            new()
+            {
+                Title = "Avatar",
+                DirectorName = "James Cameron",
+                ReleaseYear = "2009"
+            },
+            new()
+            {
+                Title = "Titanic",
+                DirectorName = "James Cameron",
+                ReleaseYear = "1997"
+            },
+            new()
+            {
+                Title = "Die Another Day",
+                DirectorName = "Lee Tamahori",
+                ReleaseYear = "2002"
+            },
+            new()
+            {
+                Title = "Godzilla",
+                DirectorName = "Gareth Edwards",
+                ReleaseYear = "2014"
+            }];
+    }
+}
