1dd3e60594140f3219876528e2e8d73892be5d3b
Lucas Garcia <lucasgarciaz2018@gmail.com>
2024-02-12 19:46:07 -0300

    feat: get movies by director's name query

---
+++ CleanArchitecture/Movies/Movies.Application/Handlers/GetMoviesByDirectorNameHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Movies.Application.Mappers;
+using Movies.Application.Queries;
+using Movies.Application.Responses;
+using Movies.Core.Repositories;
+
+namespace Movies.Application.Handlers;
+
+public class GetMoviesByDirectorNameHandler(IMovieRepository movieRepository)
+    : IRequestHandler<GetMovieByDirectorNameQuery, IEnumerable<MovieResponse>>
+{
+    private readonly IMovieRepository _movieRepository = movieRepository;
+
+    public async Task<IEnumerable<MovieResponse>> Handle(
+        GetMovieByDirectorNameQuery request,
+        CancellationToken cancellationToken
+    )
+    {
+        var movies = await _movieRepository.GetMoviesByDirectorName(request.DirectorName);
+        var movieResponses = MovieMapper.Mapper.Map<IEnumerable<MovieResponse>>(movies);
+
+        return movieResponses;
+    }
+}
---
+++ CleanArchitecture/Movies/Movies.Application/Queries/GetMovieByDirectorNameQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Movies.Application.Responses;
+
+namespace Movies.Application.Queries;
+
+public class GetMovieByDirectorNameQuery(string directorName) : IRequest<IEnumerable<MovieResponse>>
+{
+    public string DirectorName { get; set; } = directorName;
+}
