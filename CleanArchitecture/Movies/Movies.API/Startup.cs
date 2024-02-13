e36477e598496fccd22ceec4ca8b448f11df0954
Lucas Garcia <lucasgarciaz2018@gmail.com>
2024-02-12 22:22:54 -0300

    feat: addes startup settings

---
+++ CleanArchitecture/Movies/Movies.API/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Movies.Application.Handlers;
+using Movies.Core.Repositories.Base;
+using Movies.Infrastructure.Data;
+using Movies.Infrastructure.Repositores.Base;
+
+namespace Movies.API;
+
+public class Startup(IConfiguration configuration)
+{
+    private readonly IConfiguration _configuration = configuration;
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddApiVersioning();
+
+        services.AddDbContext<MovieContext>(
+            options => options.UseSqlServer(_configuration.GetConnectionString("MoviesConnection")),
+            ServiceLifetime.Singleton
+        );
+
+        services.AddSwaggerGen(config =>
+        {
+            config.SwaggerDoc(
+                name: "v1",
+                new OpenApiInfo() { Title = "Movie API Review", Version = "v1" }
+            );
+        });
+
+        services.AddAutoMapper(typeof(Startup));
+
+        services.AddMediatR(
+            config =>
+                config.RegisterServicesFromAssembly(typeof(CreateMovieCommandHandler).Assembly)
+        );
+
+        services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+    }
+}
