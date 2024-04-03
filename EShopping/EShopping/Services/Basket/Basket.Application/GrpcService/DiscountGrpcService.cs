77cc8c22dd33924cdc8f7d30309774ae4c828e3e
Lucas Garcia <lucasgarciaz2018@gmail.com>
2024-04-02 21:14:04 -0300

    feat: add discount grpc service

---
+++ EShopping/EShopping/Services/Basket/Basket.Application/GrpcService/DiscountGrpcService.cs
@@ -0,0 +1,19 @@
+﻿using Discount.Grpc.Protos;
+
+namespace Basket.Application.GrpcService;
+
+public class DiscountGrpcService
+{
+    private readonly DiscountProtoService.DiscountProtoServiceClient _serviceClient;
+
+    public DiscountGrpcService(DiscountProtoService.DiscountProtoServiceClient serviceClient)
+    {
+        _serviceClient = serviceClient;
+    }
+
+    private async Task<CouponModel> GetDiscount(string productName)
+    {
+        GetDiscountRequest discountRequest = new() { ProductName = productName };
+        return await _serviceClient.GetDiscountAsync(discountRequest);
+    }
+}
