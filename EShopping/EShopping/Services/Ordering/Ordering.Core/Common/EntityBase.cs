64dc220c1d5a6c039a9dc4a356087aa1ef9b5486
Lucas Garcia <lucasgarciaz2018@gmail.com>
2024-04-13 10:57:36 -0300

    feat: ordering projects added; feat: implementing ordering.core;

---
+++ EShopping/EShopping/Services/Ordering/Ordering.Core/Common/EntityBase.cs
@@ -0,0 +1,12 @@
+﻿namespace Ordering.Core.Common;
+
+public class EntityBase
+{
+    public int Id { get; protected set; }
+
+    //Audit properties
+    public string? CreatedBy { get; set; }
+    public DateTime? CreatedDate { get; set; }
+    public string? LastModifiedBy { get; set; }
+    public DateTime? LastModifiedDate { get; set; }
+}
