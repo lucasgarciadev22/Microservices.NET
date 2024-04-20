6615405c234335a4c0f271820315c47325aca673
Lucas Garcia <lucasgarciaz2018@gmail.com>
2024-04-20 18:32:26 -0300

    feat: created infra project event bus base implementation and classes

---
+++ EShopping/EShopping/Infrastructure/EventBus.Messages/Events/BaseIntegrationEvent.cs
@@ -0,0 +1,19 @@
+﻿namespace EventBus.Messages.Events;
+
+public class BaseIntegrationEvent
+{
+    public Guid Id { get; }
+    public DateTime CreationDate { get; set; }
+
+    public BaseIntegrationEvent()
+    {
+        Id = Guid.NewGuid();
+        CreationDate = DateTime.UtcNow;
+    }
+
+    public BaseIntegrationEvent(Guid id, DateTime creationDate)
+    {
+        Id = id;
+        CreationDate = creationDate;
+    }
+}
