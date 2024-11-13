c665bdd976bf2658023db08f7f93e9fb0432b99e
Lucas Garcia <lucasgarciaz2018@gmail.com>
2024-11-12 22:18:51 -0300

    adding elasticsearch and kibana configs

--- EShopping/EShopping/Infrastructure/Common.Logging/Logging.cs
+++ EShopping/EShopping/Infrastructure/Common.Logging/Logging.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Serilog;
+using Serilog.Events;
 using Serilog.Exceptions;
+using Serilog.Sinks.Elasticsearch;
 
 namespace Common.Logging;
 
@@ -17,20 +20,32 @@ public static class Logging
                 .Enrich.WithProperty("ApplicationName", env.ApplicationName)
                 .Enrich.WithProperty("EnvironmentName", env.EnvironmentName)
                 .Enrich.WithExceptionDetails()
-                .MinimumLevel.Override("Microsoft.AspNetCore", Serilog.Events.LogEventLevel.Warning)
-                .MinimumLevel.Override(
-                    "Microsoft.Hosting.Lifetime",
-                    Serilog.Events.LogEventLevel.Warning
-                )
+                .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Warning)
                 .WriteTo.Console();
 
             //development env overrides
             if (env.IsDevelopment())
             {
-                loggerConfig.MinimumLevel.Override("Basket", Serilog.Events.LogEventLevel.Debug);
-                loggerConfig.MinimumLevel.Override("Catalog", Serilog.Events.LogEventLevel.Debug);
-                loggerConfig.MinimumLevel.Override("Discount", Serilog.Events.LogEventLevel.Debug);
-                loggerConfig.MinimumLevel.Override("Ordering", Serilog.Events.LogEventLevel.Debug);
+                loggerConfig.MinimumLevel.Override("Basket", LogEventLevel.Debug);
+                loggerConfig.MinimumLevel.Override("Catalog", LogEventLevel.Debug);
+                loggerConfig.MinimumLevel.Override("Discount", LogEventLevel.Debug);
+                loggerConfig.MinimumLevel.Override("Ordering", LogEventLevel.Debug);
+            }
+
+            //elastic search configs
+            string? elasticUrl = context.Configuration.GetValue<string>("ElasticConfiguration:Uri");
+            if (!string.IsNullOrEmpty(elasticUrl))
+            {
+                loggerConfig.WriteTo.Elasticsearch(
+                    new ElasticsearchSinkOptions(new Uri(elasticUrl))
+                    {
+                        AutoRegisterTemplate = true,
+                        AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv8,
+                        IndexFormat = "EShopping-Logs-{0:yyyy.MM.dd}",
+                        MinimumLogEventLevel = LogEventLevel.Debug
+                    }
+                );
             }
         };
 }
